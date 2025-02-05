dbeb4e0bb647f427c91822dad6a0ac897b010237
Fraz <vfbarduco@gmail.com>
2025-02-05 11:38:55 -0300

    2.0
    05/02/2025 Introdução ao PHP

---
+++ Cinebox/includes/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CineBox</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="./assets/css/carrossel.css">
+</head>
+
+<body>
+    <header>
+        <nav>
+            <a href="#" class="logo">
+                <h1>Cine Box</h1>
+            </a>
+            <ul class="menu">
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Filmes</a></li>
+                <li><a href="#">Sobre</a></li>
+            </ul>
+
+            <ul class="menu_icones">
+                <li><a href="#">ico</a></li>
+                <li><a href="#">ico</a></li>
+                <li><a href="#">ico</a></li>
+                <li><a href="#">ico</a></li>
+            </ul>
+        </nav>
+    </header>
---
+++ Cinebox/index.php
@@ -0,0 +1,7 @@
+<?php include './includes/header.php' ?>
+
+    <section id="banner">
+        <main class="carrossel_container">
+            <img src="./assets/img/banner/banner_01.jpg" alt="banner 01">
+        </main>
+    </section>
